30a147784255dd1ca09b342c61c0c366fa46e119
TheRealBonson <bonson.ym@gmail.com>
2022-04-25 20:18:04 +1000

    added cats

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ use bevy::{
     tasks::{AsyncComputeTaskPool, logical_core_count},
 };
 use rand::prelude::*;
+use std::collections::HashSet;
+use std::sync::{Mutex};
 
 //============================================================================================================================================
 
@@ -12,6 +14,7 @@ const PHYSICS_TIME_STEP: f32 = 1. / 24.;
 const ANIMATION_TIME_STEP: f32 = 1. / 8.;
 
 const INIT_FLOCK_SIZE: u32 = 200;
+const INIT_HUNT_SIZE: u32 = 6;
 const SCALE: f32 = 2.5;
 
 const PADDING: f32 = 400.;
@@ -25,41 +28,59 @@ fn main() {
         .add_plugin(FrameTimeDiagnosticsPlugin::default())
         .insert_resource(ClearColor(Color::rgb(1.0, 1.0, 1.0)))
         .insert_resource(
-            Params {
+            FlockParams {
                 alignment_strength: 4.,
                 cohesion_strength: 1.,
                 avoidance_strength: 1.2,
                 speed: 130.,
                 radius: 60.
         })
+        .insert_resource(
+            HuntParams {
+                radius: 60.,
+                hunt_strength: 100.,
+        })
         .add_startup_system(setup)
         .add_startup_system(add_flock)
+        .add_startup_system(add_cats)
         .add_system_set(
             SystemSet::new()
                 .with_run_criteria(FixedTimestep::step(PHYSICS_TIME_STEP as f64))
                 .with_system(flocking)
                 .with_system(movement)
                 .with_system(wrapping)
+                .with_system(hunting.before(flocking))
         )
         .add_system_set(
             SystemSet::new()
                 .with_run_criteria(FixedTimestep::step(ANIMATION_TIME_STEP as f64))
                 .with_system(sprite_animation)
                 .with_system(sprite_x_direction)
-                .with_system(sprite_z_layer)
+                .with_system(sprite_z_layer.after(movement))
         )
         .run();
 }
 
 //============================================================================================================================================
 
+#[derive(Component)]
+struct Bird;
+
+#[derive(Component)]
+struct Cat;
+
 #[derive(Component, Deref, DerefMut)]
 struct Velocity(Vec2);
 
 #[derive(Component)]
-struct Boid;
+struct Hunt(Transform);
 
-struct Params {
+struct HuntParams {
+    hunt_strength: f32,
+    radius: f32,
+}
+
+struct FlockParams {
     alignment_strength: f32,
     cohesion_strength: f32,
     avoidance_strength: f32,
@@ -87,13 +108,70 @@ fn add_flock(windows: Res<Windows>, mut commands: Commands, asset_server: Res<As
 
     for _ in 1..=INIT_FLOCK_SIZE {
         commands.spawn()
-            .insert(Boid)
-            .insert(Velocity(Vec2::new(rng.gen_range(-100f32..=100f32), rng.gen_range(-100f32..=100f32)).into()))
+            .insert(Bird)
+            .insert(Velocity(Vec2::new(rng.gen_range(-1f32..=1f32), rng.gen_range(-1f32..=1f32))))
             .insert_bundle(SpriteSheetBundle {
                 transform: Transform::from_translation(Vec3::new(rng.gen_range(-bounds_x..=bounds_x), rng.gen_range(-bounds_y..=bounds_y), 1.)),
                 texture_atlas: texture_atlas_handle.clone(),
                 ..default()
             });
+    }
+}
+
+fn add_cats(windows: Res<Windows>, mut commands: Commands, asset_server: Res<AssetServer>, mut texture_atlases: ResMut<Assets<TextureAtlas>>) {
+    let mut rng = rand::thread_rng();
+
+    let bounds_x: f32 = windows.get_primary().unwrap().width() * SCALE / 2.;
+    let bounds_y: f32 = windows.get_primary().unwrap().height() * SCALE / 2.;
+
+    let texture_handle = asset_server.load("sprites/cat.png");
+    let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(200.0, 200.0), 6, 1);
+    let texture_atlas_handle = texture_atlases.add(texture_atlas);
+
+    for _ in 1..=INIT_HUNT_SIZE {
+        commands.spawn()
+            .insert(Cat)
+            .insert(Velocity(Vec2::new(rng.gen_range(-1f32..=1f32), rng.gen_range(-1f32..=1f32))))
+            .insert_bundle(SpriteSheetBundle {
+                transform: Transform::from_translation(Vec3::new(rng.gen_range(-bounds_x..=bounds_x), rng.gen_range(-bounds_y..=bounds_y), 1.)),
+                texture_atlas: texture_atlas_handle.clone(),
+                ..default()
+            });
+    }
+}
+
+fn hunting (mut commands: Commands, mut query: Query<(&mut Velocity, &Transform), With<Cat>>, prey_query: Query<(Entity, &Transform), With<Bird>>, params: Res<HuntParams>, thread_pool: Res<AsyncComputeTaskPool>) {
+    if prey_query.is_empty(){
+        return;
+    }
+    let kill_list = Mutex::new(HashSet::new());
+
+    query.par_for_each_mut(&thread_pool, logical_core_count(), |(mut velocity, transform)|{
+        let mut closest_dist = i32::MAX;
+        let mut closest_offset = Vec2::ZERO;
+
+        for (other, other_transform) in prey_query.iter() {
+            let offset = other_transform.translation.truncate() - transform.translation.truncate();
+            let dist = offset.length_squared() as i32;
+
+            if dist < closest_dist {
+                closest_dist = dist;
+                closest_offset = offset;
+
+                if (closest_dist as f32) < params.radius * params.radius {
+                    let mut kill_list = kill_list.lock().unwrap();
+                    kill_list.insert(other);
+                    break;
+                }
+            }
+        }
+
+        velocity.0 += closest_offset.normalize() * params.hunt_strength;
+    });
+
+    let kill_list = kill_list.lock().unwrap();
+    for prey_entity in kill_list.iter() {
+        commands.entity(*prey_entity).despawn();
     }
 }
 
@@ -103,14 +181,14 @@ fn movement(mut query: Query<(&mut Transform, &Velocity)>) {
     }
 }
 
-fn flocking(mut query: Query<(Entity, &mut Velocity, &Transform), With<Boid>>, params: Res<Params>, thread_pool: Res<AsyncComputeTaskPool>) {
+fn flocking(mut query: Query<(Entity, &mut Velocity, &Transform)>, params: Res<FlockParams>, thread_pool: Res<AsyncComputeTaskPool>) {
     let mut boids = Vec::new();
     for (entity, velocity, transform) in query.iter() {
         boids.push((entity.id(), velocity.0, transform.translation.truncate()));
     }
 
     query.par_for_each_mut(&thread_pool, logical_core_count(), |(entity, mut velocity, transform)| {
-        velocity.0 = velocity.0 + calculate_behaviour(entity.id(), velocity.0, transform.translation.truncate(), &boids, &params) * params.speed;
+        velocity.0 = velocity.0 + calculate_flock_behaviour(entity.id(), velocity.0, transform.translation.truncate(), &boids, &params) * params.speed;
 
         if velocity.0.length_squared() > params.speed * params.speed {
             velocity.0 = velocity.0.normalize() * params.speed;
@@ -118,7 +196,7 @@ fn flocking(mut query: Query<(Entity, &mut Velocity, &Transform), With<Boid>>, p
     }) ;
 }
 
-fn calculate_behaviour(id: u32, velocity:Vec2, position: Vec2, boids: &[(u32, Vec2, Vec2)], params: &Params) -> Vec2 {
+fn calculate_flock_behaviour(id: u32, velocity:Vec2, position: Vec2, boids: &[(u32, Vec2, Vec2)], params: &FlockParams) -> Vec2 {
     let mut alignment = Vec2::ZERO;
     let mut cohesion = Vec2::ZERO;
     let mut avoidance = Vec2::ZERO;
@@ -168,7 +246,7 @@ fn calculate_behaviour(id: u32, velocity:Vec2, position: Vec2, boids: &[(u32, Ve
     return alignment + cohesion + avoidance;
 }
 
-fn wrapping(windows: Res<Windows>, mut query: Query<&mut Transform, With<Boid>>) {
+fn wrapping(windows: Res<Windows>, mut query: Query<&mut Transform>) {
     let bounds_x: f32 = windows.get_primary().unwrap().width() * SCALE / 2.;
     let bounds_y: f32 = windows.get_primary().unwrap().height() * SCALE / 2.;
 
